e6b0f170cda7b6e563ad993a244bc14166a9b739
Carlo R. Garcia <garciaram.cal@gmail.com>
2023-11-29 00:28:26 +0800

    Login

---
+++ StudentRentals/src/Components/Login.jsx
@@ -0,0 +1,56 @@
+import React from "react";
+import '../CSS/Login.css'
+import bg from '../Images/bg-login.jpg'
+import logo from '../Images/logo.png'
+import citlogo from '../Images/citlogo.png'
+
+function Login (){
+  return (
+    <>
+    <div className="lgn-components">
+      <div className="lgn-leftside">
+        <img src={bg} alt="lgb-bg" style={{
+          filter: "blur(8px)",
+          width: "100%",
+          height: "100vh"
+        }} />
+        <img src={logo} alt="company-logo" style={{
+          position: "absolute",
+          width: "600px",
+          left: "10%",
+          top: "15%"
+        }} />
+        <h1 className="lgn-title">STUDENT RENTALS</h1>
+        <h2 className="lgn-title">MADE4EASY</h2>
+      </div>
+
+      <div className="lgn-rightside">
+        <div className="back-container">
+          <box-icon name='arrow-back' size='md'></box-icon>
+          <p>BACK</p>
+        </div>
+        <div className="lgn-container">
+          <img src={citlogo} alt="citlogo" style={{
+            width: "150px",
+            paddingTop: "50px",
+            position: "absolute",
+            top: '15%'
+          }} />
+          <p>Sign In</p>
+          <input type="text" placeholder="Username" />
+          <input type="text" placeholder="Password" />
+          <button>Login</button>
+          <hr class="solid"></hr>
+          <button>Login as Admin</button>
+          <div><h3>Don't Have an Account yet?<a href="#"
+          style={{
+
+          }}> Signup</a></h3></div>
+        </div>
+      </div>
+    </div>
+    </>
+  )
+}
+
+export default Login
