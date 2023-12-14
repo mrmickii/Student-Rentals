8d5dcd429c1484afca33b7d740e5195927620df6
trixie <trixiegailcloma24@gmail.com>
2023-12-14 23:21:11 +0800

    added Details.jsx with feedback

--- StudentRentals/src/Components/Details.jsx
+++ StudentRentals/src/Components/Details.jsx
@@ -5,6 +5,7 @@ import Header from "../Components/Header";
 import Footer from "../Components/Footer"
 import { useAuth } from "../Components/AuthContext";
 import "../CSS/Details.css"
+import { TextField } from "@mui/material";
 
 function Details() {
   const number = "09456142552";
@@ -13,6 +14,9 @@ function Details() {
   const { propId } = useParams();
   const [propertyData, setPropertyData] = useState({});
   const [loading, setLoading] = useState(true);
+  const [comments, setComments] = useState([]);
+  const [currentDate, setCurrentDate] = useState(new Date());
+  const [newComment, setNewComment] = useState('');
 
   const handleBackClick = () => {
     navigate(-1);
@@ -26,6 +30,33 @@ function Details() {
       alert("To reserve a property, you must first log in.");
     }
   };
+
+  const handleInputChange = (e) => {
+    setNewComment(e.target.value);
+};
+
+const handleSubmit = async () => {
+  try {
+      const response = await axios.post('http://localhost:8080/feedback/insertFeedback', {
+        description: newComment,
+        timestamp: currentDate
+      });
+      console.log('Comment posted:', response.data);
+    } catch (error) {
+      alert("Feedback failed");
+      console.error('Error posting comment:', error);
+    }
+      setComments([...comments, newComment]);
+      setNewComment(''); 
+};
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setCurrentDate(new Date());
+    }, 60000);
+
+    return () => clearInterval(intervalId);
+  }, []);
 
   useEffect(() => {
     const fetchPropertyDetails = async () => {
@@ -85,7 +116,26 @@ function Details() {
           <h1>Apartment Description</h1>    
           <p>
           The apartment consists of a large bright bedroom with a comfy king-sized bed, a modern fully-equipped kitchen and a sunlit living room with Apple TV and free Netflix account. It is the perfect place to stay for couples looking for a romantic location in the historic centre, within walking distance of some of the most beautiful sceneries you can find in the city and all famous landmarks. The sofa in the living room can also serve as an additional bed for a 3rd guest. Located in a side street between the ___ river bank and the leafy ___ hill park, the apartment is very quiet at night. Please note, my apartment is on the 3rd floor with no elevator. I am more than happy to help you with your luggage!  
-          </p>  
+          </p>
+          <div className="comments-section">
+              <textarea className="textarea" 
+                        placeholder="Write a comment..." 
+                        value={newComment}
+                        onChange={handleInputChange}/>
+              <button onClick={handleSubmit} className="button">Submit</button> 
+          </div>
+          <div>
+              <h2>Comments:</h2>
+          </div>
+          <div class="comments-container">
+            {comments.map((comment, index) => (
+              <div key={index} className="comments">
+                <strong>Anonymous</strong>
+                <p>{comment}</p>
+                <p>{currentDate.toLocaleString()}</p>
+              </div>
+            ))}
+          </div>  
         </div>
 
         <div className="details-body-side">
